8b840ba3fd830c51a6addd0c617f2a79d3acfe38
Lowe <0328193@students.dadeschools.net>
2018-04-10 12:05:46 -0400

    comment

--- logic.js
+++ logic.js
@@ -1,8 +1,8 @@
-var wordList = ['High School Musical','The Lion King', 'Finding Nemo']
+var moviesList = ["High School Musical", "Finding Nemo","Princess Diaries" ];
 
 var chosenWord = '';
 
-var lettersInChosenWord =[];
+var lettersInChosenWord= [];
 
 var numBlanks = 0;
 
@@ -10,7 +10,7 @@ var blanksAndSuccesses = [];
 
 var wrongGuesses = [];
 
-var letterGuess = '';
+var letterGuessed = '';
 
 
 var winCounter = 0;
@@ -18,19 +18,13 @@ var lossCounter = 0;
 var numGuesses = 9;
 
 
-
-
-
-
-
-
 function startGame() {
 
 	numGuesses = 9;
 
-	chosenword = wordList[Math.floor(Math.random() + wordList.length)];
+	chosenWord = moviesList[Math.floor(Math.random() * moviesList.length)];
 
-	lettersInChosenWord = chosenWord.split('');
+	lettersInChosenWord = chosenWord.split("");
 
 	numBlanks = lettersInChosenWord.length;
 
@@ -40,8 +34,8 @@ function startGame() {
 
 	wrongGuesses = [];
 
-	for (var i = 0, i < numBlanks, i++) {
-		blanksAndSuccesses.push("_")
+	for (var i = 0; i < numBlanks; i++) {
+		blanksAndSuccesses.push("_");
 	}
 
 	console.log(blanksAndSuccesses);
@@ -53,37 +47,36 @@ function startGame() {
 
 	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
-
 }
 
+function checkLetters(letter) {
+	var letterInWord = false;
 
-	function checkLetters(letter) {
-		var letterInWord = false;
-
-		for (var i = 0; i < numBlanks; i++) {
-			if (chosenWord[i] === letter) {
-				letterInWord = true;
-			}
+	for (var i=0; i < numBlanks; i++) {
+		if (chosenWord[i] === letter) {
+			letterInWord = true;
 		}
 	}
 
 
 	if (letterInWord) {
-		for (var j = 0; j < numBlanks; j++) {
+		for (var j=0; j < numBlanks; j++) {
 			if (chosenWord[j] === letter) {
 				blanksAndSuccesses[j] = letter;
 			}
 		}
 		console.log(blanksAndSuccesses);
-	}
+	} 
 
 	else {
 		wrongGuesses.push(letter);
 		numGuesses--;
 	}
+}
 
 function roundComplete() {
-	console.log("WinCount: " + winCounter + " | lossCount: " + lossCounter + " | numGuesses: " + numGuesses)
+	console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + 
+		" | NumGuesses: " + numGuesses);
 
 	document.getElementById("guesses-left").innerHTML = numGuesses;
 	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
@@ -108,26 +101,13 @@ function roundComplete() {
 	}
 }
 
-
-
 startGame();
-
-
-
-
-
-
-
-
 
 document.onkeyup = function(event) {
 	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
 	checkLetters(letterGuessed);
 	roundComplete();
-}
-
-
-
+};
 
 
 
